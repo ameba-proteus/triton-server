6225692d23beb9bd886596038a26c4bbd35aae8e
suguru <snamura@gmail.com>
2013-08-29 15:16:48 +0900

    Remove dependency of netty in cassandra-all.

--- src/main/java/com/amebame/triton/service/cassandra/TritonCassandraClient.java
+++ src/main/java/com/amebame/triton/service/cassandra/TritonCassandraClient.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -19,7 +20,6 @@ import org.apache.cassandra.dht.Token.TokenFactory;
 import org.apache.cassandra.utils.Hex;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jboss.netty.util.internal.ConcurrentHashMap;
 
 import com.amebame.triton.client.cassandra.method.GetColumns;
 import com.amebame.triton.client.cassandra.method.RemoveColumns;
